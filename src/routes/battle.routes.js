e71738310a769079578e62506df077a0e5aa1e17
dev-rohith <rohithv104@gmail.com>
2025-10-23 19:51:59 +0530

    final

--- src/routes/battle.routes.js
+++ src/routes/battle.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { simulateBattleController, listBattlesController } = require('../controllers/battleController');
+const { listBattlesController } = require('../controllers/battleController');
 const { authenticate } = require('../middleware/auth');
 const router = express.Router();
 
 router.use(authenticate);
 
-router.post('/', simulateBattleController);
+router.post('/', '' );
 router.get('/', listBattlesController);
 
 module.exports = router;
